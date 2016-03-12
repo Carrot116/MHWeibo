1c633232727342476bfb05175d4f049ca1d37075
Carrot <Carrot@MacMini.local>
2016-03-12 16:47:07 +0800

    empty project

---
+++ MHWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHWeiBo
+//
+//  Created by Carrot on 16/3/12.
+//  Copyright © 2016年 Carrot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
